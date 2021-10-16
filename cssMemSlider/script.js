f8af47e10f2e4348cdca52e6278d432445b54101
yuraCOM <z0632655585@gmail.com>
2021-10-16 19:28:27 +0300

    feat: add basic page  (Sut, Okt 16, 2021 19:28 PM)
    feat: add basic page  (Sut, Okt 16, 2021 19:28 PM)

---
+++ cssMemSlider/script.js
@@ -0,0 +1,22 @@
+// let data = [
+//     // dot1:{},
+//
+// ]
+//
+//
+// let dots = document.querySelector('.control').querySelectorAll('li')
+//
+// dots.forEach( item =>{
+//     item.addEventListener('click', ()=>{
+//         let sliderImg = document.querySelector('.img-slider')
+//         sliderImg.style.animation = 'moveout 2s'
+//         setTimeout(()=>{
+//             sliderImg.src = `img/${item.id[3]}.jpg`
+//             sliderImg.style.animation = 'visible 3s'
+//         }, 1000)
+//         // sliderImg.src = `img/${item.id[3]}.jpg`
+//
+//         // sliderImg.style.animation = ''
+//         console.log(item.id[3])
+//     })
+// })
